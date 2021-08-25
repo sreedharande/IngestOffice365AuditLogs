05fd2d86a73c049be02eead5d165e265dc5acd87
Sreedhar Ande <andesreedhar@gmail.com>
2021-08-25 13:45:19 -0700

    Fixed parsing issue

--- IngestOffice365Data/AzFunctionO365Data/run.ps1
+++ IngestOffice365Data/AzFunctionO365Data/run.ps1
@@ -44,151 +44,140 @@ if (-Not [string]::IsNullOrEmpty($LAUri)){
 }
 
 function Write-OMSLogfile {
-    <#
-    .SYNOPSIS
-    Inputs a hashtable, date and workspace type and writes it to a Log Analytics Workspace.
-    .DESCRIPTION
-    Given a  value pair hash table, this function will write the data to an OMS Log Analytics workspace.
-    Certain variables, such as Customer ID and Shared Key are specific to the OMS workspace data is being written to.
-    This function will not write to multiple OMS workspaces.  Build-signature and post-analytics function from Microsoft documentation
-    at https://docs.microsoft.com/en-us/azure/log-analytics/log-analytics-data-collector-api
-    .PARAMETER DateTime
-    date and time for the log.  DateTime value
-    .PARAMETER Type
-    Name of the logfile or Log Analytics "Type".  Log Analytics will append _CL at the end of custom logs  String Value
-    .PARAMETER LogData
-    A series of key, value pairs that will be written to the log.  Log file are unstructured but the key should be consistent
-    withing each source.
-    .INPUTS
-    The parameters of data and time, type and logdata.  Logdata is converted to JSON to submit to Log Analytics.
-    .OUTPUTS
-    The Function will return the HTTP status code from the Post method.  Status code 200 indicates the request was received.
-    .NOTES
-    Version:        2.0
-    Author:         Travis Roberts
-    Creation Date:  7/9/2018
-    Purpose/Change: Crating a stand alone function.
-    .EXAMPLE
-    This Example will log data to the "LoggingTest" Log Analytics table
-    $type = 'LoggingTest'
-    $dateTime = Get-Date
-    $data = @{
-        ErrorText   = 'This is a test message'
-        ErrorNumber = 1985
-    }
-    $returnCode = Write-OMSLogfile $dateTime $type $data -Verbose
-    write-output $returnCode
-    #>
-        [cmdletbinding()]
-        Param(
-            [Parameter(Mandatory = $true, Position = 0)]
-            [datetime]$dateTime,
-            [parameter(Mandatory = $true, Position = 1)]
-            [string]$type,
-            [Parameter(Mandatory = $true, Position = 2)]
-            [psobject]$logdata,
-            [Parameter(Mandatory = $true, Position = 3)]
-            [string]$CustomerID,
-            [Parameter(Mandatory = $true, Position = 4)]
-            [string]$SharedKey
-        )
-        Write-Verbose -Message "DateTime: $dateTime"
-        Write-Verbose -Message ('DateTimeKind:' + $dateTime.kind)
-        Write-Verbose -Message "Type: $type"
-        write-Verbose -Message "LogData: $logdata"
-        
-		# Supporting Functions
-		
-        # Function to create the auth signature
-        function Build-signature ($CustomerID, $SharedKey, $Date, $ContentLength, $method, $ContentType, $resource) {
-            $xheaders = 'x-ms-date:' + $Date
-            $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
-            $bytesToHash = [text.Encoding]::UTF8.GetBytes($stringToHash)
-            $keyBytes = [Convert]::FromBase64String($SharedKey)
-            $sha256 = New-Object System.Security.Cryptography.HMACSHA256
-            $sha256.key = $keyBytes
-            $calculateHash = $sha256.ComputeHash($bytesToHash)
-            $encodeHash = [convert]::ToBase64String($calculateHash)
-            $authorization = 'SharedKey {0}:{1}' -f $CustomerID,$encodeHash
-            return $authorization
-        }
-		
-        # Function to create and post the request
-        Function Post-LogAnalyticsData ($CustomerID, $SharedKey, $Body, $Type) {
-            $method = "POST"
-            $ContentType = 'application/json'
-            $resource = '/api/logs'
-            $rfc1123date = ($dateTime).ToString('r')
-            $ContentLength = $Body.Length
-            $signature = Build-signature `
-                -customerId $CustomerID `
-                -sharedKey $SharedKey `
-                -date $rfc1123date `
-                -contentLength $ContentLength `
-                -method $method `
-                -contentType $ContentType `
-                -resource $resource
-			            
-			# Compatible with previous version and supports both Azure Commercial and Azure Gov
-			if ([string]::IsNullOrEmpty($LAUri)){
-				$uri = "https://" + $CustomerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
-			}
-			else
-			{
-				$uri = $LAURI + $resource + "?api-version=2016-04-01"
-			}
-			
-            $headers = @{
-                "Authorization" = $signature;
-                "Log-Type" = $type;
-                "x-ms-date" = $rfc1123date
-                "time-generated-field" = $dateTime
-            }
-            $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $ContentType -Headers $headers -Body $body -UseBasicParsing
-            Write-Verbose -message ('Post Function Return Code ' + $response.statuscode)
-            return $response.statuscode
-        }
-
-        # Check if time is UTC, Convert to UTC if not.
-        # $dateTime = (Get-Date)
-        if ($dateTime.kind.tostring() -ne 'Utc'){
-            $dateTime = $dateTime.ToUniversalTime()
-            Write-Verbose -Message $dateTime
-        }
-
-        # Add DateTime to hashtable
-        #$logdata.add("DateTime", $dateTime)
-        $logdata | Add-Member -MemberType NoteProperty -Name "DateTime" -Value $dateTime
-
-        #Build the JSON file
-        $logMessage = ($logdata | ConvertTo-Json -Depth 20)
-        Write-Verbose -Message $logMessage
-
-        #Submit the data
-        #$returnCode = Post-LogAnalyticsData -CustomerID $CustomerID -SharedKey $SharedKey -Body ([System.Text.Encoding]::UTF8.GetBytes($logMessage)) -Type $type
-        $returnCode = Post-LogAnalyticsData -CustomerID $CustomerID -SharedKey $SharedKey -Body $logMessage -Type $type
-        Write-Verbose -Message "Post Statement Return Code $returnCode"
-        return $returnCode
-    }
-
-function Get-AuthToken{
-    [cmdletbinding()]
-        Param(
-            [Parameter(Mandatory = $true, Position = 0)]
-            [string]$ClientID,
-            [parameter(Mandatory = $true, Position = 1)]
-            [string]$ClientSecret,
-            [Parameter(Mandatory = $true, Position = 2)]
-            [string]$tenantdomain,
-            [Parameter(Mandatory = $true, Position = 3)]
-            [string]$TenantGUID
-        )
-    
-    $body = @{grant_type="client_credentials";resource=$OfficeLoginUri;client_id=$ClientID;client_secret=$ClientSecret}
-    $oauth = Invoke-RestMethod -Method Post -Uri $AzureAADLoginUri/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
-    $headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
-    return $headerParams 
+<#
+.SYNOPSIS
+Inputs a hashtable, date and workspace type and writes it to a Log Analytics Workspace.
+.DESCRIPTION
+Given a  value pair hash table, this function will write the data to an OMS Log Analytics workspace.
+Certain variables, such as Customer ID and Shared Key are specific to the OMS workspace data is being written to.
+This function will not write to multiple OMS workspaces.  Build-signature and post-analytics function from Microsoft documentation
+at https://docs.microsoft.com/en-us/azure/log-analytics/log-analytics-data-collector-api
+.PARAMETER DateTime
+date and time for the log.  DateTime value
+.PARAMETER Type
+Name of the logfile or Log Analytics "Type".  Log Analytics will append _CL at the end of custom logs  String Value
+.PARAMETER LogData
+A series of key, value pairs that will be written to the log.  Log file are unstructured but the key should be consistent
+withing each source.
+.INPUTS
+The parameters of data and time, type and logdata.  Logdata is converted to JSON to submit to Log Analytics.
+.OUTPUTS
+The Function will return the HTTP status code from the Post method.  Status code 200 indicates the request was received.
+.NOTES
+Version:        2.0
+Author:         Travis Roberts
+Creation Date:  7/9/2018
+Purpose/Change: Crating a stand alone function.
+.EXAMPLE
+This Example will log data to the "LoggingTest" Log Analytics table
+$type = 'LoggingTest'
+$dateTime = Get-Date
+$data = @{
+	ErrorText   = 'This is a test message'
+	ErrorNumber = 1985
 }
+$returnCode = Write-OMSLogfile $dateTime $type $data -Verbose
+write-output $returnCode
+#>
+	[cmdletbinding()]
+	Param(
+		[Parameter(Mandatory = $true, Position = 0)]
+		[datetime]$dateTime,
+		[parameter(Mandatory = $true, Position = 1)]
+		[string]$type,
+		[Parameter(Mandatory = $true, Position = 2)]
+		[psobject]$logdata,
+		[Parameter(Mandatory = $true, Position = 3)]
+		[string]$CustomerID,
+		[Parameter(Mandatory = $true, Position = 4)]
+		[string]$SharedKey
+	)
+	
+	Write-Verbose -Message "DateTime: $dateTime"
+	Write-Verbose -Message ('DateTimeKind:' + $dateTime.kind)
+	Write-Verbose -Message "Type: $type"
+	write-Verbose -Message "LogData: $logdata"
+	
+	#region Supporting Functions
+	
+	# Function to create the auth signature
+	function Build-signature ($CustomerID, $SharedKey, $Date, $ContentLength, $method, $ContentType, $resource) {
+		$xheaders = 'x-ms-date:' + $Date
+		$stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+		$bytesToHash = [text.Encoding]::UTF8.GetBytes($stringToHash)
+		$keyBytes = [Convert]::FromBase64String($SharedKey)
+		$sha256 = New-Object System.Security.Cryptography.HMACSHA256
+		$sha256.key = $keyBytes
+		$calculateHash = $sha256.ComputeHash($bytesToHash)
+		$encodeHash = [convert]::ToBase64String($calculateHash)
+		$authorization = 'SharedKey {0}:{1}' -f $CustomerID,$encodeHash
+		return $authorization
+	}
+	
+	# Function to create and post the request
+	function Post-LogAnalyticsData ($CustomerID, $SharedKey, $Body, $Type) {
+		$method = "POST"
+		$ContentType = 'application/json'
+		$resource = '/api/logs'
+		$rfc1123date = ($dateTime).ToString('r')
+		$ContentLength = $Body.Length
+		$signature = Build-signature `
+			-customerId $CustomerID `
+			-sharedKey $SharedKey `
+			-date $rfc1123date `
+			-contentLength $ContentLength `
+			-method $method `
+			-contentType $ContentType `
+			-resource $resource
+					
+		# Compatible with previous version and supports both Azure Commercial and Azure Gov
+		if ([string]::IsNullOrEmpty($LAUri)){
+			$uri = "https://" + $CustomerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+		}
+		else
+		{
+			$uri = $LAURI + $resource + "?api-version=2016-04-01"
+		}
+		
+		$headers = @{
+			"Authorization" = $signature;
+			"Log-Type" = $type;
+			"x-ms-date" = $rfc1123date
+			"time-generated-field" = $dateTime
+		}
+		$response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $ContentType -Headers $headers -Body $body
+		Write-Verbose -message ('Post Function Return Code ' + $response.statuscode)
+		return $response.statuscode
+	}      
+
+	#endregion
+	
+	#Submit the data        
+	$returnCode = Post-LogAnalyticsData -CustomerID $CustomerID -SharedKey $SharedKey -Body $logdata -Type $type
+	Write-Verbose -Message "Post Statement Return Code $returnCode"
+	return $returnCode
+}
+
+function Get-AuthToken {
+	[cmdletbinding()]
+	Param(
+		[Parameter(Mandatory = $true, Position = 0)]
+		[string]$ClientID,
+		[parameter(Mandatory = $true, Position = 1)]
+		[string]$ClientSecret,
+		[Parameter(Mandatory = $true, Position = 2)]
+		[string]$tenantdomain,
+		[Parameter(Mandatory = $true, Position = 3)]
+		[string]$TenantGUID
+	)
+
+	$body = @{grant_type="client_credentials";resource=$OfficeLoginUri;client_id=$ClientID;client_secret=$ClientSecret}
+	$oauth = Invoke-RestMethod -Method Post -Uri $AzureAADLoginUri/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+	$headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+
+	return $headerParams 
+}
+
 
 function SendToLogA { 
 	[cmdletbinding()]
@@ -224,6 +213,22 @@ function SendToLogA {
     }
 }
 
+function Convert-ObjectToHashTable {
+	[CmdletBinding()]
+	param
+	(
+		[parameter(Mandatory=$true,ValueFromPipeline=$true)]
+		[pscustomobject] $Object
+	)
+	$HashTable = @{}
+	$ObjectMembers = Get-Member -InputObject $Object -MemberType *Property
+	foreach ($Member in $ObjectMembers) 
+	{
+		$HashTable.$($Member.Name) = $Object.$($Member.Name)
+	}
+	return $HashTable
+}																																				   
+
 function Get-O365Data{
     [cmdletbinding()]
     Param(
@@ -247,19 +252,20 @@ function Get-O365Data{
 	
 	#Loop for each content Type like Audit.General; DLP.ALL
     foreach($contentType in $contentTypes){
+		$contentType = $contentType.Trim()
         $listAvailableContentUri = "$OfficeLoginUri/api/v1.0/$tenantGUID/activity/feed/subscriptions/content?contentType=$contentType&PublisherIdentifier=$AADAppPublisher&startTime=$startTime&endTime=$endTime"
-        Write-Host "***************************************"
-		Write-Host $listAvailableContentUri
+        
+		Write-Output $listAvailableContentUri
 		
 		do {
             #List Available Content
             $contentResult = Invoke-RestMethod -Method GET -Headers $headerParams -Uri $listAvailableContentUri
-            $contentResult.Count
+            Write-Output $contentResult.Count
             #Loop for each Content
             foreach($obj in $contentResult){
                 #Retrieve Content
                 $data = Invoke-RestMethod -Method GET -Headers $headerParams -Uri ($obj.contentUri)
-                $data.Count
+                Write-Output $data.Count
                 #Loop through each Record in the Content
                 foreach($event in $data){
                     #Filtering for Recrord types
@@ -267,20 +273,20 @@ function Get-O365Data{
                     if($Office365RecordTypes -eq "0"){
                         #We dont need Cloud App Security Alerts due to MCAS connector
                         if(($event.Source) -ne "Cloud App Security"){
-                            #Write each event to Log A
-                            $writeResult = SendToLogA $event $Office365CustomLog
-                            #$writeResult
+                            $ht = Convert-ObjectToHashTable $event     
+                            $ht = $ht | ConvertTo-Json -Depth 5                            
+                            $writeResult = SendToLogA $ht $Office365CustomLog
                         }
                     }
-                    else{
+                    else {
                         #Get only certain record types
                         $types = ($Office365RecordTypes).split(",")
                         if(($event.RecordType) -in $types){
                             #We dont need Cloud App Security Alerts due to MCAS connector
                             if(($event.Source) -ne "Cloud App Security"){
-                                #write each event to Log A
-                                $writeResult = SendToLogA $event $Office365CustomLog
-                                #$writeResult
+                                $ht = Convert-ObjectToHashTable $event     
+                                $ht = $ht | ConvertTo-Json -Depth 5                            
+                                $writeResult = SendToLogA $ht $Office365CustomLog
                             }
                         }
                         
@@ -294,7 +300,9 @@ function Get-O365Data{
                 $nextPage = $true
                 $listAvailableContentUri = $nextPageResult.Headers.NextPageUrl
             }
-            Else{$nextPage = $false}
+            Else {
+				$nextPage = $false
+			}
         } until ($nextPage -eq $false)
     }
 }
